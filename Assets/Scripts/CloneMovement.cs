fb7ac9ff4b3251aec8ecad6f662951a0f68cee5f
Parker Verboom <parker.verboom@gmail.com>
2023-02-04 01:44:53 -0400

    Player Movement

---
+++ Assets/Scripts/CloneMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CloneMovement : MonoBehaviour
+{
+    private Rigidbody2D _cloneRigidbody;
+    private Rigidbody2D _playerReference;
+    
+    public Vector2 playerVelocity;
+    public bool isBlush;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cloneRigidbody = GetComponent<Rigidbody2D>();
+        _playerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        while (!isBlush)
+        {
+            _cloneRigidbody.velocity = _playerReference.velocity;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isBlush = true;
+        }
+    }
+}
