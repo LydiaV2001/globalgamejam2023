c13f309ee562117d1ef233f6537eb8f49883e247
Parker Verboom <parker.verboom@gmail.com>
2023-02-04 09:35:43 -0400

    Fix player blush

--- Assets/Scripts/CloneMovement.cs
+++ Assets/Scripts/CloneMovement.cs
@@ -28,13 +28,22 @@ public class CloneMovement : MonoBehaviour
         }
     }
 
-    /*
-    private void OnTriggerStay2D(Collider2D other)
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             isBlush = true;
+            _cloneRigidbody.velocity = new Vector2(0, 0);
         }
     }
-    */
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isBlush = false;
+        }
+    }
+    
 }
