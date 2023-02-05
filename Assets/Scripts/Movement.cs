c1aabce29e389a4c6fd8b829488dc8562803f5aa
Parker Verboom <parker.verboom@gmail.com>
2023-02-05 10:24:55 -0400

    player animation, first level

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
@@ -11,18 +12,37 @@ public class Movement : MonoBehaviour
     
     public float playerSpeed = 3;
 
+    public Animator animator;
+
+    public SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         _playerRigidbody = GetComponent<Rigidbody2D>();
     }
     
     void FixedUpdate()
     {
         _playerRigidbody.velocity = _movementInput.normalized * playerSpeed;
+
+        animator.SetFloat("speed", Mathf.Abs(_movementInput.x) + Mathf.Abs(_movementInput.y));
     }
     
     void OnMove(InputValue movementValue) {
         _movementInput = movementValue.Get<Vector2>();
+        
+        if (_movementInput.x < 0)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        
+        
+
     }
 }
