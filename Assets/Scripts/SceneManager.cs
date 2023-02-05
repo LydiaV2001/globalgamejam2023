cbc082d60ec52919351f63cca6b100da20cc9159
Badhri Nadh Arja <badhrinadharja@t9f45.wpa.dal.ca>
2023-02-05 11:01:00 -0400

    Timer

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject cloneGates;
+    [SerializeField] private GameObject cloneGates;
+
+    [SerializeField] private GameObject clock;
+
+    [SerializeField] private Timer timer;
+
+    [SerializeField] private int levelTime;
+
+    [SerializeField] private GameObject info;
 
     private int _cloneCount;
     public  int clonesNeeded = 3;
+
+    private void Start()
+    {
+        Invoke(nameof(StartGame), 3.0f);
+    }
 
     public void CloneCounter()
     {
@@ -18,6 +31,13 @@ public class SceneManager : MonoBehaviour
         {
             cloneGates.SetActive(true);
         }
+    }
+
+    public void StartGame()
+    {
+        info.SetActive(false);
+        timer.timeValue = levelTime;
+        clock.SetActive(true);
     }
 
 
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [HideInInspector]
+    public float timeValue;
+    
+    private TMP_Text _timerText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _timerText = gameObject.GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        _timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
